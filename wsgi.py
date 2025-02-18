db79443c4da66094b7f54c6cbe58edd0dba83ff4
Jack Hodges <jackhodges32@hotmail.co.uk>
2025-02-18 19:25:57 +0000

    Completed 2.3 last step

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,7 @@
 
 from flask import Flask, render_template, request
 from longest_word.game import Game
+from wsgi import app
 
 
 app = Flask(__name__)
@@ -24,3 +25,9 @@ def check():
     word = request.form['word']
     is_valid = game.is_valid(word)
     return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process will serve the app
+    app.run(host='127.0.0.1', port=8080, debug=True)
